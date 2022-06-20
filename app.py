9217cacf4a85850c1fc82589a685f56cd9177ef9
Sai <shein.htet.sai@sap.com>
2022-06-20 12:38:38 +0800

    made json API

--- app.py
+++ app.py
@@ -12,15 +12,14 @@ def home():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    text = [request.form['sentence']]
-    print(text)
-    result = model.predict(text)
-    print(result)
-    return render_template('index.html', result = result[0])
+    data = request.json
+    text = data['data']
+    result =  model.predict([text])
+    return_dict = {}
+    return_dict['result'] = result[0]
+    print(return_dict)
+    return return_dict
 
 port = int(os.getenv("PORT",0))
 if __name__ == '__main__':
-    if port != 0:
-        app.run(debug=True, host='0.0.0.0',port=port)
-    else:
-        app.run(debug=True)
+    app.run(host='0.0.0.0', port = 8080)
